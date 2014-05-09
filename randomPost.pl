d938f109af923dc491bc9166417abc0a8b64f198
U-TSUBAME\syu <syu@TSUBAME.(none)>
2014-05-09 19:14:36 +0900

    randomPost.pl is modified

--- randomPost.pl
+++ randomPost.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/perl
 
+
+=head1 NAME
+
+post twitter timeline
+
+=head1 SYNOPSIS
+
+randomPost [options] 
+
+ Options:
+   --dry-run         dryrun
+   --postdata        direct post
+   --postfile        filename(default postData.xml)
+
+=cut
+
 use strict;
 use warnings;
 use utf8;
@@ -9,12 +25,35 @@ use Data::Dumper;
 use XML::Simple;
 use Getopt::Long;
 
+use Pod::Usage;
+use Getopt::Long qw/:config posix_default no_ignore_case bundling auto_help/;
+
+
 use update;
 
 #コマンドラインオプション
-my $dry_run = 0;
+GetOptions(
+        \my %opts, qw/
+        dry-run
+        postdata=s
+        postfile=s
+        /
+        ) or pod2usage(verbose => 0);
 
-GetOptions('dry-run' => \$dry_run );
+my $dry_run = $opts{'dry-run'};
+
+#コマンドライン入力されたデータをpostする
+if($opts{postdata}){
+	#dry run(実際にはpostしない)
+	if($dry_run){
+		print encode_utf8($opts{postdata});
+		exit;
+	}
+	myUpdateStatus::updateStatus(status => $opts{postdata});
+	exit;
+
+}
+
 
 #postするデータファイルを読む
 my $postDataFileName = "./postData.xml";
@@ -27,13 +66,13 @@ my $postData = $postDataXs->XMLin($postDataFileName);
 
 
 
-#print Dumper $postData;
-
 my $dataSize       = $postData->{dataSize};
 my $randomPostData = $postData->{randomPostData}{data};
 
 my $dataNo = int(rand($dataSize));
 my $text = $$randomPostData[$dataNo];
+
+#dry run(実際にはpostしない)
 if($dry_run){
 	print encode_utf8($text);
 	exit;
